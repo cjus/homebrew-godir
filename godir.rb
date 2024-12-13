4be278adfb7492ea9e50ff49be13eb038b1b446b
cjus <carlos.justiniano@gmail.com>
2024-12-13 07:54:43 -0700

    updated godir.rb

--- godir.rb
+++ godir.rb
@@ -2,7 +2,7 @@ class Godir < Formula
   desc "A fuzzy directory navigation tool for command line Ninjas"
   homepage "https://github.com/cjus/godir"
   url "https://github.com/cjus/godir/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"  # You'll need to calculate this
+  sha256 "3d30479cdd49c782786dcc9828ea6fc8b89f9bea507572dbfb54eb75fbaf7f27"
   license "MIT"
 
   depends_on "rust" => :build
