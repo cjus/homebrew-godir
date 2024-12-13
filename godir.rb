3067dd17ebbc23ed27fe9fd0cd170ae9616198b9
cjus <carlos.justiniano@gmail.com>
2024-12-13 08:07:42 -0700

    Updated version

--- godir.rb
+++ godir.rb
@@ -1,8 +1,8 @@
 class Godir < Formula
   desc "A fuzzy directory navigation tool for command line Ninjas"
   homepage "https://github.com/cjus/godir"
-  url "https://github.com/cjus/godir/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "3d30479cdd49c782786dcc9828ea6fc8b89f9bea507572dbfb54eb75fbaf7f27"
+  url "https://github.com/cjus/godir/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "8e36af66203d6ac039c57e102ced5918172efd4a8047bb66d023d758db648a43i"
   license "MIT"
 
   depends_on "rust" => :build
